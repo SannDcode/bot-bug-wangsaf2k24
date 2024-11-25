298605fdc9047fdc18cbd178c12e4ea724cc6229
sanndcode <maulanaabdul1354@gmail.com>
2024-11-25 23:15:40 +0700

    coba coba

---
+++ slide/adiwConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'approval': {
+    'num': '62883173814158',
+    'text': "setujui",
+    'set': "👀 Harus mendapatkan persetujuan oleh creator script, Jika ingin memakai script ini",
+    'greet': "*Disetujui Oleh Creator, Silahkan Restart Panel Atau Run Ulang script* ☠️ "
+  },
+  'creatorScript': '62883173814158',
+  'filePath': "./slide/vocado.co",
+  'checkFilePath': "../avocado.js",
+  'codeToDetect': 'main();'
+};
